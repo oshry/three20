08145ed2fab89b7454ccaec829b39296ef5261eb
Rodrigo Mazzilli <rodrigo@wellknown.as>
2009-08-29 07:56:24 -0300

    TTTableUpdateItem & TTTableUpdateItemCell added

--- src/Three20/TTTableItemCell.h
+++ src/Three20/TTTableItemCell.h
@@ -44,6 +44,21 @@
   TTImageView* _imageView2;
 }
 
+
+@property(nonatomic,readonly,retain) UILabel* titleLabel;
+@property(nonatomic,readonly) UILabel* captionLabel;
+@property(nonatomic,readonly,retain) UILabel* timestampLabel;
+@property(nonatomic,readonly,retain) TTImageView* imageView2;
+
+@end
+
+@interface TTTableUpdateItemCell : TTTableLinkedItemCell {
+	UILabel* _titleLabel;
+	UILabel* _timestampLabel;
+	TTImageView* _imageView2;
+}
+
+
 @property(nonatomic,readonly,retain) UILabel* titleLabel;
 @property(nonatomic,readonly) UILabel* captionLabel;
 @property(nonatomic,readonly,retain) UILabel* timestampLabel;
