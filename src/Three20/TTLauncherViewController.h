e4c742b431548d0a5bc66626901a9779170be894
Rodrigo Mazzilli <rodrigo@wellknown.as>
2010-03-02 20:30:45 -0800

    Added header and footer view attributes.

--- src/Three20/TTLauncherViewController.h
+++ src/Three20/TTLauncherViewController.h
@@ -4,11 +4,15 @@
 //
 //  Created by Rodrigo Mazzilli on 9/25/09.
 
+#import "Three20/TTView.h"
 #import "Three20/TTViewController.h"
 #import "Three20/TTLauncherView.h"
 
+
 @interface TTLauncherViewController : TTViewController <UINavigationControllerDelegate> {
 	UIView *_overlayView;
+	TTView *_headerView;
+	TTView *_footerView;
 	TTLauncherView *_launcherView;
 	UINavigationController *_launcherNavigationController;
 	UIViewController *_launcherNavigationControllerTopViewController;
@@ -16,5 +20,7 @@
 @property(nonatomic, retain) UINavigationController *launcherNavigationController;
 @property(nonatomic, readonly) TTLauncherView *launcherView;
 @property(nonatomic, retain) UIViewController *launcherNavigationControllerTopViewController;
+@property(nonatomic, retain) TTView *headerView;
+@property(nonatomic, retain) TTView *footerView;
 
 @end
